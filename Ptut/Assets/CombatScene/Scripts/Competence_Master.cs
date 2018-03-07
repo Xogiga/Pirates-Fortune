262930008900fa6270c894d5e691478b36c4f0b6
Youmustkidding <32686212+Youmustkidding@users.noreply.github.com>
2018-03-07 20:41:40 +0100

    Infobulle et formatage
    Les infobulles fonctionnent correctement et les noms des compétences et des sorts sont formatés.

--- Ptut/Assets/CombatScene/Scripts/Competence_Master.cs
+++ Ptut/Assets/CombatScene/Scripts/Competence_Master.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Competence_Master : MonoBehaviour {
-	private GameObject infobulle;																				//Ne marche pas à cause du collider ?
+	private GameObject infobulle;
 
-	void OnEnable(){
-		infobulle = GameObject.Find("Hero(Clone)/Hero Stats Canvas/Barre des competences/Strike/Infobulle");
-		infobulle.SetActive (false);
+	private void Find_Infobulle(int Numero_competence){
+		infobulle = GameObject.Find("Hero(Clone)/Hero Stats Canvas/Barre des competences/Competence_"+Numero_competence+"/Infobulle_"+Numero_competence);
 	}
 
-	void OnMouseEnter (){
-		
+	public void Show_infobulle(int Numero_competence){
+		Find_Infobulle(Numero_competence);
 		infobulle.SetActive (true);
 	}
 
-	void OnMouseExit()
-	{
+	public void Hide_infobulle(int Numero_competence){
 		infobulle.SetActive (false);
 	}
+
 }
+
+
+
