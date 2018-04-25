1ef4a4d0af31a721523d1096c2c68920cc4c62fb
Tristan <voyanui0697@gmail.com>
2018-04-25 16:00:56 +0200

    Trouver point proche (scene map)
    Ajout de la fonction pour qu'un point trouve les points proches au mouse over

--- Ptut/Assets/MapScene/Script/interest_marker_script.cs
+++ Ptut/Assets/MapScene/Script/interest_marker_script.cs
@@ -3,7 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class interest_marker_script : MonoBehaviour {
-	
-	void OnMouseOver(){
+	List<GameObject> liste_points;
+
+	void OnEnable() {
+		liste_points = null;
+	}
+
+	void OnMouseEnter(){
+		if (liste_points == null) {
+			ChercherPointsProche ();
+		}
+		ChangerCouleur ();
+	}
+
+
+	void OnMouseExit(){
+		if (liste_points != null) {
+			foreach (GameObject g in liste_points) {
+				g.GetComponent<SpriteRenderer> ().color = new Color32 (255, 255, 255, 255);
+			}
+		}
+	}
+
+
+	void ChercherPointsProche(){
+		liste_points = new List<GameObject> ();
+		int rayon = 5;
+		this.GetComponent<Collider> ().enabled = false;
+		Collider[] liste_colliders = null;
+		while (liste_colliders == null) {
+			liste_colliders = Physics.OverlapSphere (this.transform.position, rayon);
+			rayon++;
+		}
+		this.GetComponent<Collider> ().enabled = true;
+		if (rayon == 6) {
+			foreach (Collider c in liste_colliders) {
+				liste_points.Add (c.gameObject);
+			}
+		} else {
+			liste_points.Add (liste_colliders [0].gameObject);
+		}
+			
+	}
+
+	private void ChangerCouleur(){
+		foreach (GameObject g in liste_points) {
+			g.GetComponent<SpriteRenderer> ().color = new Color32 (255, 255, 255, 50);
+		}
 	}
 }
