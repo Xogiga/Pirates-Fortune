61082e2587f3881a7c782fe31f601342b6e5e999
Tristan <voyanui0697@gmail.com>
2018-05-04 16:17:28 +0200

    Lignes entre les points

--- Ptut/Assets/MapScene/Script/GameManager_Master.cs
+++ Ptut/Assets/MapScene/Script/GameManager_Master.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 namespace MapScene {
 	public class GameManager_Master : MonoBehaviour {
 		public GameObject interest_point;
+		public GameObject line;
+		public List<GameObject> liste_point;
+		public List<GameObject> liste_lignes;
+		private GameObject map;
 
 		void OnEnable(){
+			map = GameObject.FindGameObjectWithTag ("Map");
 			create_point ();
+			verif_map ();
+
 		}
 
 		//Fonction qui dispere aléatoirement les points d'intérêts sur la map
 		private void create_point(){
-			GameObject map = GameObject.FindGameObjectWithTag ("Map");
 			List<Tuple> cases = new List<Tuple> ();
 
 			for (float i = -6; i < 21; i+=3) {												//Crée une liste de case
@@ -31,7 +37,9 @@ namespace MapScene {
 				if (Random.Range (0f, 1f) < chance) {										//Fait apparaître un point quand le chiffre random est inférieur à la chance d'apparition
 					float posX = Random.Range (c.x, c.x + 2);
 					float posY = Random.Range (c.y, c.y + 2);
-					Instantiate (interest_point, new Vector3 (posX, posY, 1), Quaternion.identity, map.transform);
+					GameObject nouvpoint = Instantiate (interest_point, new Vector3 (posX, posY, 1), Quaternion.identity, map.transform);
+					nouvpoint.name = "Point" + liste_point.Count;
+					liste_point.Add (nouvpoint);
 					chance -= 0.1f;															//Si un point apparaît les chances d'apparition du suivant sont plus faible
 				} else {
 					chance += 0.1f;															//S'il n'apparaît pas l'inverse
@@ -39,5 +47,56 @@ namespace MapScene {
 				cpt_case++;
 			}
 		}
+
+		private void verif_map(){
+			/*liste_point [3].GetComponent<SpriteRenderer> ().flipY = true;
+			liste_point [liste_point.Count - 3].GetComponent<SpriteRenderer> ().flipX = true;
+			liste_point [liste_point.Count - 3].GetComponent<SpriteRenderer> ().flipY = true;*/
+
+			foreach (GameObject g in liste_point) {
+				create_line (g);
+			}
+		}
+
+		private void create_line(GameObject centre){
+			int rayon = 5;
+			centre.GetComponent<Collider> ().enabled = false;
+			Collider[] liste_colliders = Physics.OverlapSphere (centre.transform.position, rayon);
+			while (liste_colliders.Length==0) {
+				rayon++;
+				liste_colliders = Physics.OverlapSphere (centre.transform.position, rayon);
+			}
+			centre.GetComponent<Collider> ().enabled = true;
+
+
+			foreach (Collider c in liste_colliders) {
+				if (verif_list_lines ("Line" + c.gameObject.name + centre.name)) {
+					creer_ligne (centre, c);
+				}
+			}
+		}
+
+		private bool verif_list_lines(string futur_nom){
+			foreach (GameObject line in liste_lignes) {
+				if (line.name == futur_nom) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private void creer_ligne(GameObject centre, Collider c){
+			GameObject nouvelle_ligne = Instantiate (line, Vector3.zero, Quaternion.identity, map.transform);
+			LineRenderer linerenderer = nouvelle_ligne.GetComponent<LineRenderer> ();
+			linerenderer.SetPosition (0, centre.transform.position);
+			linerenderer.SetPosition (1, c.transform.position);
+			linerenderer.name = "Line" + centre.name + c.gameObject.name;
+			liste_lignes.Add (linerenderer.gameObject);
+			centre.GetComponent<interest_marker_script> ().liste_lignes_proche.Add (nouvelle_ligne);
+			c.gameObject.GetComponent<interest_marker_script> ().liste_lignes_proche.Add (nouvelle_ligne);
+
+			nouvelle_ligne.SetActive (true);
+		}
 	}
 }
+	
--- Ptut/Assets/MapScene/Script/interest_marker_script.cs
+++ Ptut/Assets/MapScene/Script/interest_marker_script.cs
@@ -3,52 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class interest_marker_script : MonoBehaviour {
-	List<GameObject> liste_points;
+	public List<GameObject> liste_lignes_proche;
 
 	void OnEnable() {
-		liste_points = null;
+		liste_lignes_proche = new List<GameObject>();
 	}
 
 	void OnMouseEnter(){
-		if (liste_points == null) {
-			ChercherPointsProche ();
-		}
-		ChangerCouleur ();
+		Show_Lines ();
 	}
 
 
 	void OnMouseExit(){
-		if (liste_points != null) {
-			foreach (GameObject g in liste_points) {
-				g.GetComponent<SpriteRenderer> ().color = new Color32 (255, 255, 255, 255);
-			}
+		Show_Lines ();
+	}
+
+	private void Show_Lines(){
+		foreach (GameObject g in liste_lignes_proche) {
+			g.SetActive (!g.activeSelf);
 		}
 	}
 
 
-	void ChercherPointsProche(){
-		liste_points = new List<GameObject> ();
-		int rayon = 5;
-		this.GetComponent<Collider> ().enabled = false;
-		Collider[] liste_colliders = null;
-		while (liste_colliders == null) {
-			liste_colliders = Physics.OverlapSphere (this.transform.position, rayon);
-			rayon++;
-		}
-		this.GetComponent<Collider> ().enabled = true;
-		if (rayon == 6) {
-			foreach (Collider c in liste_colliders) {
-				liste_points.Add (c.gameObject);
-			}
-		} else {
-			liste_points.Add (liste_colliders [0].gameObject);
-		}
-			
-	}
-
-	private void ChangerCouleur(){
-		foreach (GameObject g in liste_points) {
-			g.GetComponent<SpriteRenderer> ().color = new Color32 (255, 255, 255, 50);
-		}
-	}
 }
